a543fb5fb861424c1dcf42c0b4c8a3086f90fb8a
pujitha <muttinenipujitha@gmail.com>
2024-08-13 21:35:44 +0530

    Initial commit

---
+++ src/pages/cart/checkout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Checkout = () => {
+  return (
+    <div>
+      <h1 style={{textAlign:"center",color:"purple"}}>Payment Successs...Continue Shopping!!!!</h1>
+    </div>
+  )
+}
+
+
